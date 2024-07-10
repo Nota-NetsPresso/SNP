7f88884d3a788928c74ed76bae142ed34313722d
kyunghwan.shim <eric4991@naver.com>
2024-07-10 04:45:20 +0000

    initial commit

---
+++ SNP_compression/compress.py
@@ -0,0 +1,61 @@
+import os
+import json
+import torch
+
+from netspresso import NetsPresso
+from netspresso.enums import CompressionMethod, GroupPolicy, LayerNorm, Policy
+from netspresso.clients.compressor.v2.schemas import Options
+
+
+from utils.utils import tofx
+
+COMPRESS_RATIO_PATH="./SNP_compression/compress_ratio"
+COMPRESS_RATIO={
+    "deit_tiny_patch16_224":"DeiT_t.json",
+    "deit_small_patch16_224":"DeiT_s.json",
+    "deit_base_patch16_224":"DeiT_b.json"
+}
+
+def snp(args, model, inputs):
+    netspresso = NetsPresso(email=args.NetsPresso_Email, password=args.NetsPresso_Pwd)
+    
+    model = tofx(model)
+    orig_model_path = os.path.join(args.output_dir, "original_model.pt")
+    compress_model_path = os.path.join(args.output_dir, "compressed")
+
+    torch.save(model, orig_model_path)
+
+    # 1. Declare compressor
+    compressor = netspresso.compressor_v2()
+
+    # 2. Upload model
+    model = compressor.upload_model(
+        input_model_path=orig_model_path,
+        input_shapes=[{"batch": 1, "channel": 3, "dimension": [224, 224]}],
+    )
+
+    # 3. Select compression method
+    compression_info = compressor.select_compression_method(
+        model_id=model.ai_model_id,
+        compression_method=CompressionMethod.PR_SNP,
+        options=Options(
+            policy=Policy.AVERAGE,
+            layer_norm=LayerNorm.TSS_NORM,
+            group_policy=GroupPolicy.NONE,
+            reshape_channel_axis=-1,
+        ),
+    )
+
+    # 4. load compress ratio
+    with open(os.path.join(COMPRESS_RATIO_PATH, COMPRESS_RATIO[args.model]), "r") as j_file:
+        compress_ratio = json.load(j_file)
+
+    for available_layer in compression_info.available_layers:
+        available_layer.values = [compress_ratio[available_layer.name]]
+
+    # 5. Compress model
+    compressed_model_info = compressor.compress_model(
+        compression=compression_info,
+        output_dir=compress_model_path,
+    )
+    return torch.load(compressed_model_info.compressed_model_path)
---
+++ main.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+read -p "Please enter your NetsPresso Email: " USER_NAME
+read -s -p "Please enter your NetsPresso Password: " USER_PWD
+read -p "Please enter the path to your ImageNet dataset: " IMAGENET_PATH
+
+echo
+
+export USER_NAME
+export USER_PWD
+export IMAGENET_PATH
+
+export OUPUT_DIR="./output"
+
+python compress.py --NetsPresso-Email ${USER_NAME} \
+                    --NetsPresso-Pwd ${USER_PWD} \
+                    --model deit_tiny_patch16_224 \
+                    --data-path ${IMAGENET_PATH}\
+                    --output_dir ${OUPUT_DIR} \
+                    --num-imgs-snp-calculation 64\
+
+# Check if compress.py ran successfully
+if [ $? -ne 0 ]; then
+  echo "compress.py failed to execute. Exiting."
+  exit 1
+fi
+
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7\
+    python -m torch.distributed.launch --nproc_per_node 8 --master_addr="127.0.0.1" --master_port=12345 \
+        train.py --model "${OUPUT_DIR}/compressed/compressed.pt" \
+                 --batch-size 256 \
+                 --epochs 20 \
+                 --output_dir ${OUPUT_DIR} \
+                 --data-path  ${IMAGENET_PATH}\
+                 > ./txt_logs/training_test.txt 2>&1 &
+
+# Check if train.py ran successfully
+if [ $? -ne 0 ]; then
+  echo "train.py failed to execute. Exiting."
+  exit 1
+fi
+
+echo "Both scripts ran successfully."
